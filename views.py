afa85bfbc57cdf681442d5a984ebea90c1ab80f8
sangho <sangho@onlab.us>
2020-04-10 17:55:19 +0900

    SONA connection

--- views.py
+++ views.py
@@ -8,6 +8,9 @@ from edgetron.serializers import CatalogSerializer
 
 import requests
 
+sona_server_IP = "10.2.1.33"
+sona_url = "http://" + sona_server_IP + ":8181/onos/openstacknetworking/"
+sona_headers = {'Content-Type': 'application/json', 'Authorization': 'Basic b25vczpyb2Nrcw=='}
 
 @csrf_exempt
 def catalog_list(request):
@@ -49,9 +52,29 @@ def catalog_onboard(request, pk):
         return JsonResponse(serializer.data, status=200)
 
 
+@csrf_exempt
+def application_instantiate(request, pk):
+    """""
+    Instantiate Application
+    """""
+    try:
+        catalog = Catalog.objects.get(pk=pk)
+    except Catalog.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CatalogSerializer(data=data)
+        if serializer.is_valid():
+            send_createport_request()
+        else:
+            return JsonResponse(serializer.errors, status=400)
+
+        return JsonResponse(serializer.data, status=200)
+
+
 def send_network_request():
-    url = "http://127.0.0.1:8000/sona/v2.0/networks"
-    headers = {'Content-Type': 'application/json'}
+    url = sona_url + "networks"
     payload = {
         'name': 'net1',
         'provider:network_type': 'vlan',
@@ -59,4 +82,21 @@ def send_network_request():
         'provider:segmentation_id': 2,
         'provider:tenant_id': 1
     }
-    r = requests.put(url, headers=headers, data=payload)
+    r = requests.put(url, headers=sona_headers, data=payload)
+
+
+def send_createport_request():
+    url = sona_url + "ports"
+    payload = {
+        "port": {
+            "name": "private-port",
+            "network_id": "a87cc70a-3e15-4acf-8205-9b711a3531b7",
+            "fixed_ips": [
+                {
+                    "ip_address": "12.12.11.12"
+                }
+            ],
+            "tenant_id": 1
+        }
+    }
+    r = requests.put(url, headers=sona_headers, data=payload)
