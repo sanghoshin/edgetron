4d89c1c3961e6b8438ff15ba744dff0c0415d7b1
sangho <sangho@onlab.us>
2020-05-19 16:45:56 +0900

    Adding more fields in creating network and subnet

--- views.py
+++ views.py
@@ -43,19 +43,26 @@ def kubernetes_cluster(request):
         if serializer.is_valid():
 
             network_id = str(uuid.uuid4())
-            r = send_network_request(network_id)
+            segment_id = 1
+            tenant_id = str(uuid.uuid4())
+            r = send_network_request(network_id, segment_id, tenant_id)
             if r.status_code != 201:
                 return JsonResponse(r.text, safe=False)
+
             subnet_id = str(uuid.uuid4())
-            r = send_subnet_request(network_id, subnet_id)
+            cidr = "10.10.1.0/24"
+            start = "10.10.1.2"
+            end = "10.10.1.255"
+            gateway = "10.10.1.1"
+            r = send_subnet_request(network_id, subnet_id, tenant_id, cidr, start, end, gateway)
             if r.status_code != 201:
                 return JsonResponse(r.text, safe=False)
+
             port_id = str(uuid.uuid4())
             ip_address = "10.10.1.2"
-            tenant_id = str(uuid.uuid4())
-            r = send_createport_request(network_id, port_id, ip_address, tenant_id)
-            if r.status_code != 201:
-                return JsonResponse(r.text, safe=False)
+            # r = send_createport_request(network_id, port_id, ip_address, tenant_id)
+            # if r.status_code != 201:
+            #    return JsonResponse(r.text, safe=False)
         else:
             return JsonResponse(serializer.errors, status=400)
 
@@ -85,30 +92,49 @@ def deployment_application(request, pk):
         return JsonResponse(serializer.data, status=200)
 
 
-def send_subnet_request(network_id, subnet_id):
+def send_subnet_request(network_id, subnet_id, tenant_id, cidr, start_ip, end_ip, gateway):
     url = sona_url + "subnets"
     payload = {
         "subnet": {
             "id": subnet_id,
+            "allocation_pools": [
+                {
+                    "start": start_ip,
+                    "end": end_ip
+                }
+            ],
+            "cidr": cidr,
+            "host_routes": [],
+            "subnetpool_id": "null",
+            "enable_dhcp": "true",
             "name": "k8s VM subnet",
             "network_id": network_id,
+            "tenant_id": tenant_id,
             "ip_version": 4,
-            "cidr": "192.168.199.0/24"
+            "cidr": "192.168.199.0/24",
+            "ip_version": 4,
+            "gateway_ip": gateway,
         }
     }
     r = requests.post(url, headers=sona_headers, json=payload)
     return r
 
 
-def send_network_request(network_id):
+def send_network_request(network_id, segment_id, tenant_id):
     url = sona_url + "networks"
     payload = {
         "network": {
+            "status": "ACTIVE",
+            "subnets": [],
             "id": network_id,
-            "name": "sample_network",
+            "provider:segmentation_id": segment_id,
+            "is_default": "false",
+            "port_security_enabled": "true",
+            "name": "k8s_vm_network",
+            "tenant_id": tenant_id,
             "admin_state_up": "true",
-            "dns_domain": "my-domain.org",
-            "mtu": 1400
+            "provider:network_type": "vxlan",
+            "mtu": 1450
         }
     }
     r = requests.post(url, headers=sona_headers, json=payload)
